ab0225af497db71fd44cc8a731390abe36025734
Ra-Ha-Til <rahatil880@gmail.com>
2026-09-04 18:15:33 +0600

    hello

---
+++ MiniCalculator.py
@@ -0,0 +1,21 @@
+
+a = float(input("Enter the first number:"))
+b = int(input("Enter the 2nd number:"))
+op = input("Which you want to do:(+, -, *, /, **, %):")
+
+if op == '+':
+    print(a+b)
+elif op == "-":
+    print(a-b)
+elif op == "-":
+    print(a-b)
+elif op == "*":
+    print(a*b)
+elif op == "/":
+    print(a/b)
+elif op == "**":
+    print(a**b)
+elif op == "%":
+    print(a%b)
+else:
+    print("Syntax Error")
